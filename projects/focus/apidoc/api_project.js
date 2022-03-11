8a55546cb5c39bc930a9749eae995a2d555b715f
k2archer <pirate.kwei@gmail.com>
2022-03-11 20:30:08 +0800

    update apidoc

---
+++ projects/focus/apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Focus Tomato",
+  "version": "0.0.0",
+  "description": "接口文档",
+  "title": "Focus Tomato 接口文档",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2022-03-10T13:39:41.964Z",
+    "url": "http://apidocjs.com",
+    "version": "0.24.0"
+  }
+});
